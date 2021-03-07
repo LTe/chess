92bc2c97f60ab9fb8f5d593a239e42fd2e11ace9
Piotr Niełacny <piotr.nielacny@gmail.com>
2021-03-07 15:43:12 +0100

    Update lessons with comments

--- website/sidebars.auto.js
+++ website/sidebars.auto.js
@@ -145,15 +145,7 @@ module.exports = {
         ]
     },
     "blokowanie_skoczka",
-    "dwa_gonce",
-    "krol_i_2x_wieza",
-    "krol_i_hetman",
-    "krol_i_wieza",
-    "krol_i_wieza_hetman",
-    "manewr_trojkata",
     "przelom",
-    "roszada",
-    "tylko_pionek",
     "zasady_debiutowe"
 ]
 };
